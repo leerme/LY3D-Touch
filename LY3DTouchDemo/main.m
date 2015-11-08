fd5302bcfa59a746e98a61e663f9464ce4183e25
leiyu <642459351@qq.com>
2015-11-08 16:20:43 +0800

    First Commit

---
+++ LY3DTouchDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LY3DTouchDemo
+//
+//  Created by qianfeng on 15/11/7.
+//  Copyright © 2015年 lei yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
